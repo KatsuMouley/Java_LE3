b65f7e7b6b1bf5e45dc771ae3743fe951190c1fc
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-24 22:22:42 -0300

    Exercise 4

--- exercise4/ConvertToReal.java
+++ exercise4/ConvertToReal.java
@@ -1,5 +1,15 @@
-package exercise4;
+package exercise4; 
 
 public class ConvertToReal {
+    double cotacaoDoDolar, money;
+
+    public ConvertToReal(double cDol, double myMoney){
+        this.cotacaoDoDolar = cDol;
+        this.money = myMoney;
+    }
     
+    public double toReal(){
+        return cotacaoDoDolar * money;
+    }
+
 }
--- exercise4/Ex4.java
+++ exercise4/Ex4.java
@@ -2,5 +2,15 @@ package exercise4;
 import java.util.Scanner;
 public class Ex4{
     public static void run(){
+        Scanner input = new Scanner(System.in);
+        double cotacaoDolar, money;
+        System.out.println("Qual a cotação do Dolar(R$)?");
+        cotacaoDolar = input.nextDouble();
+        System.out.println("Quantos dolares você tem($)?");
+        money = input.nextDouble();
+        input.nextLine();
+        ConvertToReal conversion = new ConvertToReal(cotacaoDolar, money);
+        System.out.println("Você tem "+ conversion.toReal()+ "R$ em sua conta");
+        
     }
 }
