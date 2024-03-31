313790fd0f79b452ec80046daf909e679a57940d
KatsuMouley <vitorluizsantosmougenot@gmail.com>
2024-03-31 02:30:37 -0300

    Exercise 21

--- exercise21/Ex21.java
+++ exercise21/Ex21.java
@@ -3,5 +3,12 @@ import java.util.Scanner;
 
 public class Ex21 {
     public static void run(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Qual a idade do nadador?");
+        int age = input.nextInt();
+        input.nextLine();
+        Nadador swim = new Nadador(age);
+        System.out.println("A categoria que o nadador cai é a de "+swim.categoria());
+        input.close();
     }
 }
